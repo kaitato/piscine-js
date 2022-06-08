b5db7076daa43bbeab6eeb023710b87fa0c32987
karyun.cheung <karyun.cheung@gmail.com>
2022-06-08 14:47:55 +0100

    Fix if first index = sep

--- unbreakable.js
+++ unbreakable.js
@@ -2,7 +2,7 @@ const split = (str,sep) => {
     let arr = []
     for (let i = 0 ; i < str.length ; i++) {
         let newstr = ""
-        if (str[0] === sep[0]) {
+        if (str[i] === sep[0]) {
             arr.push("")
             arr.push(sep)
             i++

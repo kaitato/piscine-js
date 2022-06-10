47a8006e8d63c35ab4cc120fa402af79e4484028
karyun.cheung <karyun.cheung@gmail.com>
2022-06-10 19:36:52 +0100

    vowel dot

---
+++ vowel-dots.js
@@ -0,0 +1,14 @@
+var vowels = /[aeiou|AEIOU]/g
+const vowelDots = (str) => {
+    let vowelsOfStr = str.match(vowels)
+    if (vowelsOfStr == undefined) {
+        return str
+    }
+    for (let i = 0; i < vowelsOfStr.length; i++) {
+        str = str.replace(vowelsOfStr[i], vowelsOfStr[i]+".")
+    }
+
+    return str
+}
+
+console.log(vowelDots("hello"))

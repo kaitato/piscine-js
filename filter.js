c8d9ccd8f6e0c08cba8062bbc8a443c4da38ec5c
karyun.cheung <karyun.cheung@gmail.com>
2022-06-15 18:54:38 +0100

    filter

--- filter.js
+++ filter.js
@@ -1,25 +1,32 @@
-function map(arr,func){
-    let res = []
-    let i = 0
-    while(i < arr.length){
-        let  ina = 0
-        ina = func(arr[i],i,arr)
-        res.push(ina)
-        i++
-    }
-    return res
-}
-function flatMap(arr, fun){
-    let res = []
-    let mapres = map(arr,fun)
-    for (let map of mapres) {
-        if (Array.isArray(map)) {
-            for (let m of map) {
-                res.push(m)
-            }
-        } else {
-            res.push(map)
+function filter(arr,func){
+    let res =[]
+    for(let i =0;i<arr.length;i++){
+        if(func(arr[i],i,arr)){
+            res.push(arr[i])
         }
     }
     return res
-}   
+}
+function reject(arr,func){
+    let res =[]
+    for(let i =0;i<arr.length;i++){
+        if(!func(arr[i],i,arr)){
+            res.push(arr[i])
+        }
+    }
+    return res
+}
+function partition(arr,func){
+    let resTrue = []
+    let resFalse = []
+    for(let i =0;i<arr.length;i++){
+        if(func(arr[i],i,arr)){
+            resTrue.push(arr[i])
+        }
+        if(!func(arr[i],i,arr)){
+            resFalse.push(arr[i])
+        }
+    }
+    let res2x = [resTrue,resFalse]
+    return res2x
+}

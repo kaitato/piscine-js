ab4d1f9de7d205ba259c9ad7c774aa9a30c4a83c
karyun.cheung <karyun.cheung@gmail.com>
2022-06-16 20:02:04 +0100

    get them al

--- get-them-all.js
+++ get-them-all.js
@@ -1,27 +1,27 @@
 export const getArchitects = () => {
     let arr1 = [Array.from(document.getElementsByTagName('a'))]
     let arr2 = [Array.from(document.getElementsByTagName('span'))]
-    let arr = [arr1 + arr2]
+    let arr = [arr1, arr2]
     return arr
 }
 
 export const getClassical = () => {
     let arr1 = [Array.from(document.querySelectorAll('a.classical'))]
     let arr2 = [Array.from(document.querySelectorAll('a:not(.classical)'))]
-    let arr = [arr1 + arr2]
+    let arr = [arr1, arr2]
     return arr
 }
 
 export const getActive = () => {
     let arr1 = [Array.from(document.getElementsByClassName('classical active'))]
     let arr2 = [Array.from(document.querySelectorAll('.active:not(.classical)'))]
-    let arr = [arr1 + arr2]
+    let arr = [arr1, arr2]
     return arr
 }
 
 export const getBonannoPisano = () => {
     let arr1 = [Array.from(document.querySelectorAll('#BonannoPisano'))]
     let arr2 = [Array.from(document.querySelectorAll('a.classical.active:not(#BonannoPisano)'))]
-    let arr = [arr1 + arr2]
+    let arr = [arr1, arr2]
     return arr
 }

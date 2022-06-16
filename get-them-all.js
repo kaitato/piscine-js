116fc441006ec5fd10dc8486abc3deff4675cf2b
karyun.cheung <karyun.cheung@gmail.com>
2022-06-16 20:01:21 +0100

    get them al

--- get-them-all.js
+++ get-them-all.js
@@ -5,8 +5,23 @@ export const getArchitects = () => {
     return arr
 }
 
-export const getClassical = () => {}
+export const getClassical = () => {
+    let arr1 = [Array.from(document.querySelectorAll('a.classical'))]
+    let arr2 = [Array.from(document.querySelectorAll('a:not(.classical)'))]
+    let arr = [arr1 + arr2]
+    return arr
+}
 
-export const getActive = () => {}
+export const getActive = () => {
+    let arr1 = [Array.from(document.getElementsByClassName('classical active'))]
+    let arr2 = [Array.from(document.querySelectorAll('.active:not(.classical)'))]
+    let arr = [arr1 + arr2]
+    return arr
+}
 
-export const getBonannoPisano = () => {}
+export const getBonannoPisano = () => {
+    let arr1 = [Array.from(document.querySelectorAll('#BonannoPisano'))]
+    let arr2 = [Array.from(document.querySelectorAll('a.classical.active:not(#BonannoPisano)'))]
+    let arr = [arr1 + arr2]
+    return arr
+}

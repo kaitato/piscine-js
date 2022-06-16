be4a95b0a711bf9aad21e10f8b5b8c80813f047e
karyun.cheung <karyun.cheung@gmail.com>
2022-06-16 20:34:46 +0100

    bonanno

--- get-them-all.js
+++ get-them-all.js
@@ -8,7 +8,7 @@ export const getArchitects = () => {
 
 export const getClassical = () => {
     let arr1 = Array.from(document.querySelectorAll('a.classical'))
-    let arr2 = Array.from(document.querySelectorAll('classical:not(.a)'))
+    let arr2 = Array.from(document.querySelectorAll('a:not(.classical)'))
     return [arr1].concat([arr2])
 }
 

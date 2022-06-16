7ddadc68b07da576bec333e4572fdd62319fbcab
karyun.cheung <karyun.cheung@gmail.com>
2022-06-16 18:30:39 +0100

    get

--- get-them-all.js
+++ get-them-all.js
@@ -1,7 +1,3 @@
 const getArchitects = () => {
-    let arr = []
-    let arr1 = [Array.from(document.getElementsByTagName('a'))]
-    let arr2 = [Array.from(document.getElementsByTagName('span'))]
-    arr.push(arr1, arr2)
-    return arr
+    let arr1 = [Array.from(document.getElementsByTagName('a'))] + [Array.from(document.getElementsByTagName('span'))]
 }

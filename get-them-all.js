8b0394a57ff43785e78755f51327425560975624
karyun.cheung <karyun.cheung@gmail.com>
2022-06-16 20:27:09 +0100

    bonanno

--- get-them-all.js
+++ get-them-all.js
@@ -1,27 +1,25 @@
 export const getArchitects = () => {
-    let arr1 = [Array.from(document.getElementsByTagName('a'))]
-    let arr2 = [Array.from(document.getElementsByTagName('span'))]
-    let arr = [arr1, arr2]
-    return arr
+    let arr1 = Array.from(document.getElementsByTagName('a'))
+    let arr2 = Array.from(document.getElementsByTagName('span'))
+    return [arr1].concat([arr2])
+    // console.log(arr)
+
 }
 
 export const getClassical = () => {
-    let arr1 = [Array.from(document.querySelectorAll('a.classical'))]
-    let arr2 = [Array.from(document.querySelectorAll('a:not(.classical)'))]
-    let arr = [arr1, arr2]
-    return arr
+    let arr1 = Array.from(document.querySelectorAll('a.classical'))
+    let arr2 = Array.from(document.querySelectorAll('a:not(.classical)'))
+    return [arr1].concat([arr2])
 }
 
 export const getActive = () => {
-    let arr1 = [Array.from(document.getElementsByClassName('classical active'))]
-    let arr2 = [Array.from(document.querySelectorAll('.active:not(.classical)'))]
-    let arr = [arr1, arr2]
-    return arr
+    let arr1 = Array.from(document.getElementsByClassName('classical active'))
+    let arr2 = Array.from(document.querySelectorAll('.active:not(.classical)'))
+    return [arr1].concat([arr2])
 }
 
 export const getBonannoPisano = () => {
-    let arr1 = [Array.from(document.querySelectorAll('#BonannoPisano'))]
-    let arr2 = [Array.from(document.querySelectorAll('a.classical.active:not(#BonannoPisano)'))]
-    let arr = [arr1, arr2]
-    return arr
+    let arr1 = Array.from(document.querySelectorAll('#BonannoPisano'))
+    let arr2 = Array.from(document.querySelectorAll('a.classical.active:not(#BonannoPisano)'))
+    return [arr1].concat([arr2])
 }

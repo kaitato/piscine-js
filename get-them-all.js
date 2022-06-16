9401eb53f294f529ecf08db7c5f2488bc1efb258
karyun.cheung <karyun.cheung@gmail.com>
2022-06-16 18:59:58 +0100

    get them al

--- get-them-all.js
+++ get-them-all.js
@@ -1,3 +1,12 @@
-const getArchitects = () => {
-    [Array.from(document.getElementsByTagName('a'))].concat([Array.from(document.getElementsByTagName('span'))])
+export const getArchitects = () => {
+    let arr1 = [Array.from(document.getElementsByTagName('a'))]
+    let arr2 = [Array.from(document.getElementsByTagName('span'))]
+    let arr = [arr1 + arr2]
+    return arr
 }
+
+export const getClassical = () => {}
+
+export const getActive = () => {}
+
+export const getBonannoPisano = () => {}

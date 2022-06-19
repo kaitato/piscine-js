ba7f8708ebc9b8cfe6b6b36a138e345a76ba70da
karyun.cheung <karyun.cheung@gmail.com>
2022-06-19 03:58:17 +0100

    fifty shades of cold

--- fifty-shades-of-cold.js
+++ fifty-shades-of-cold.js
@@ -1,38 +1,28 @@
-import {colors} from './data.js';
-export function generateClasses(){
-    let style = document.createElement('style')
-    style.innerHTML =  ''
-    for(let i = 0; i < colors.length;i++){
-        style.innerHTML +=  '.' + colors[i] + '{background:' + colors[i] + ';}'
-    }
-    document.getElementsByTagName('head')[0].appendChild(style);
+import {colors} from './data.js'
 
+export const generateClasses = () => {
+    let el = document.createElement('style')
+    for (let i of colors) {
+        el.textContent += `.${i} {
+            background: ${i};
+        }\n` 
+    }
+    document.head.appendChild(el)
 }
-export function generateColdShades(){
-    let view = ['aqua', 'blue', 'turquoise', 'green', 'cyan', 'navy', 'purple']
-    for(let i = 0;i<colors.length;i++){
-        for(let j = 0;j<view.length;j++){
-            let Regex = new RegExp(view[j])
-            let bol = colors[i].match(Regex)
-            if(bol !== null){
-                let div = document.createElement('div')
-                // document.head.classList.add(colors[i])
-                div.className = colors[i]
-                div.textContent = colors[i]
-                document.body.append(div)
-            }
+
+export const generateColdShades = () => {
+    for (let i of colors) {
+        if (i.includes('aqua') || i.includes('blue') || i.includes('turquoise') || i.includes('green') || i.includes('cyan') || i.includes('navy') || i.includes('purple')) {
+            let el = document.createElement('div')
+            el.className = i
+            el.textContent = `${i}`
+            document.body.appendChild(el)
         }
     }
 }
-export function choseShade(d){
-    let elem = document.querySelectorAll('div')
-    for(let i = 0;i<colors.length;i++){
-        let style = elem[i].className
-        let flag = elem[i].classList.contains(d)
-        if(!flag){
-            elem[i].classList.remove(style)
-            elem[i].classList.add(d)
-        } 
+
+export const choseShade = (color) => {
+    for (let i of document.querySelectorAll('div')) {
+        i.className = color
     }
-    
 }

d210db189e30e09fdb4b37e54725447249b045d2
karyun.cheung <karyun.cheung@gmail.com>
2022-06-20 20:13:34 +0100

    get rid of console log

--- pick-omit.js
+++ pick-omit.js
@@ -2,8 +2,6 @@ const pick = (object, string) => {
     let keys = Object.keys(object);
     let filteredKeys = keys.filter(key => typeof string === "string" ? string === key : string.includes(key))
     let e = {}
-    console.log("filteredkeys: ", filteredKeys)
-    console.log("keys: ", keys)
     filteredKeys.forEach((filteredKey) => e[filteredKey] = object[filteredKey])
     return e
 }
@@ -12,8 +10,6 @@ const omit = (object, string) => {
     let keys = Object.keys(object);
     let filteredKeys = keys.filter(key => typeof string === "string" ? !(string === key) : !(string.includes(key)))
     let e = {}
-    console.log("filteredkeys: ", filteredKeys)
-    console.log("keys: ", keys)
     filteredKeys.forEach((filteredKey) => e[filteredKey] = object[filteredKey])
     return e
 }

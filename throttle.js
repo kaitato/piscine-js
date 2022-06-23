ea8bb199a7e6dc267be075ad0a0f38e6505430d1
karyun.cheung <karyun.cheung@gmail.com>
2022-06-23 19:05:36 +0100

    throttle

--- throttle.js
+++ throttle.js
@@ -1,5 +1,32 @@
-const throttle = (f, delay) => {
+const debounce = (f, ms) => {
+    let timeout
+    return function executedFunction() {
+      const context = this
+      const args = arguments
+      const later = function() {
+        timeout = null
+        f.apply(context, args)
+      }
+      clearTimeout(timeout)
+      timeout = setTimeout(later, ms)
+    }
 }
-  
-const opThrottle = (f, delay, options = {}) => {
-}
+
+const opDebounce = (f,ms,op) => {
+    let timeout
+    return function executedFunction() {
+        const context = this
+        const args = arguments
+        const later = function() {
+        timeout = null
+        if (op !== undefined) if (!op.leading) f.apply(context, args)
+        }
+        let callNow
+        if (op !== undefined) {
+            callNow = op.leading && !timeout
+        }
+        clearTimeout(timeout)
+        timeout = setTimeout(later, ms)
+        if (callNow) f.apply(context, args)
+    }
+} 

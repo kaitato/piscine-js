a1881e19ab790f7052dea8d159642b5d08cc0e9a
karyun.cheung <karyun.cheung@gmail.com>
2022-07-06 16:20:31 +0100

    pyra

--- pyramid.js
+++ pyramid.js
@@ -1,13 +1,13 @@
 const pyramid = (char,depth) => {
     let result = ""
     for (let i = 0; i < depth; i++) {
-        for (let a = 0; a < depth; a++) {
+        for (let a = 0; i < depth; a++) {
             result += ' '
         }
         for (let p = 0; p < (2 * i) + 1; p++) {
             result += char
         }
-        for (let b = 0; b < depth; b++) {
+        for (let b = 0; i < depth; b++) {
             result += ' '
         }
         if (i !== depth - 1) {

014694993a5639e51b890d828ee6b1698c4ab739
karyun.cheung <karyun.cheung@gmail.com>
2022-07-06 16:35:11 +0100

    wtf

--- pyramid.js
+++ pyramid.js
@@ -2,7 +2,9 @@ const pyramid = (char,depth) => {
     let result = ""
     for (let i = 0; i < depth; i++) {
         for (let a = i; a < depth - 1; a++) {
-            result += " "
+            for (let l = 0; l < char.length; l++) {
+                result += " "
+            }
         }
         for (let p = 0; p < (2 * i) + 1; p++) {
             result += char

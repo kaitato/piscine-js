92acc0ca029e873060381cf5347c67ae48758137
karyun.cheung <karyun.cheung@gmail.com>
2022-07-06 15:53:16 +0100

    change

--- triangle.js
+++ triangle.js
@@ -1,7 +1,7 @@
 const triangle = (char,depth) => {
     let result = ""
-    for (i = 0; i < depth; i++) {
-        for (a = 0; a < i + 1; a++) {
+    for (let i = 0; i < depth; i++) {
+        for (let a = 0; a < i + 1; a++) {
             result += char
         }
         result += "\n"

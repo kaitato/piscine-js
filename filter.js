878423a35ec28629125fbb94c153fc13334a033e
karyun.cheung <karyun.cheung@gmail.com>
2022-07-14 01:29:24 +0100

    filter

--- filter.js
+++ filter.js
@@ -1,32 +1,32 @@
-function filter(arr,func){
-    let res =[]
-    for(let i =0;i<arr.length;i++){
-        if(func(arr[i],i,arr)){
-            res.push(arr[i])
+function filter(arr, func) {
+    let res = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (func(arr[i], i, arr)) {
+            res.push(arr[i]);
         }
     }
-    return res
+    return res;
 }
-function reject(arr,func){
-    let res =[]
-    for(let i =0;i<arr.length;i++){
-        if(!func(arr[i],i,arr)){
-            res.push(arr[i])
+function reject(arr, func) {
+    let res = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (!func(arr[i], i, arr)) {
+            res.push(arr[i]);
         }
     }
-    return res
+    return res;
 }
-function partition(arr,func){
-    let resTrue = []
-    let resFalse = []
-    for(let i =0;i<arr.length;i++){
-        if(func(arr[i],i,arr)){
-            resTrue.push(arr[i])
+function partition(arr, func) {
+    let resTrue = [];
+    let resFalse = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (func(arr[i], i, arr)) {
+            resTrue.push(arr[i]);
         }
-        if(!func(arr[i],i,arr)){
-            resFalse.push(arr[i])
+        if (!func(arr[i], i, arr)) {
+            resFalse.push(arr[i]);
         }
     }
-    let res2x = [resTrue,resFalse]
-    return res2x
+    let res2x = [resTrue, resFalse];
+    return res2x;
 }

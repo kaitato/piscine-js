e2d606a7e9d96f27d84a88ae79d363ab5a80ec46
karyun.cheung <karyun.cheung@gmail.com>
2022-07-14 01:20:09 +0100

    for each

--- for-each.js
+++ for-each.js
@@ -1,7 +1,7 @@
 const forEach = (arr, func) => {
-	let res = []
-	for (let i = 0; i < arr.length; i++){
-		res.push(func(arr[i], i, arr))
-	}
-	return res
-}
+    let res = [];
+    for (let i = 0; i < arr.length; i++) {
+        res.push(func(arr[i], i, arr));
+    }
+    return res;
+};

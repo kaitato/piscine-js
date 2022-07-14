386e256e52f544cfdc00f9e5a5774faad81d0a2e
karyun.cheung <karyun.cheung@gmail.com>
2022-07-14 01:30:01 +0100

    accumulator

--- reduce.js
+++ reduce.js
@@ -1,34 +1,34 @@
-function fold(arr,func,accumulator=0){
-    for(let i = 0;i<arr.length;i++){
-        accumulator = (func(accumulator,arr[i]))
+function fold(arr, func, accumulator = 0) {
+    for (let i = 0; i < arr.length; i++) {
+        accumulator = func(accumulator, arr[i]);
     }
-    return accumulator
+    return accumulator;
 }
-function foldRight(arr,func,accumulator=0){
-    for(let i = arr.length-1;i>=0;i--){
-        accumulator = (func(accumulator,arr[i]))
+function foldRight(arr, func, accumulator = 0) {
+    for (let i = arr.length - 1; i >= 0; i--) {
+        accumulator = func(accumulator, arr[i]);
     }
-    return accumulator
+    return accumulator;
 }
-function reduce(arr,func,accumulator){
-    if(typeof arr[0] === 'number'){
-        accumulator = 0
-    }else {
-        accumulator = ''
+function reduce(arr, func, accumulator) {
+    if (typeof arr[0] === "number") {
+        accumulator = 0;
+    } else {
+        accumulator = "";
     }
-    for(let i = 0;i<arr.length;i++){
-        accumulator = (func(accumulator,arr[i]))
+    for (let i = 0; i < arr.length; i++) {
+        accumulator = func(accumulator, arr[i]);
     }
-    return accumulator
+    return accumulator;
 }
-function reduceRight(arr,func,accumulator){
-    if(typeof arr[0] === 'number'){
-        accumulator = 0
-    }else {
-        accumulator = ''
+function reduceRight(arr, func, accumulator) {
+    if (typeof arr[0] === "number") {
+        accumulator = 0;
+    } else {
+        accumulator = "";
     }
-    for(let i = arr.length-1;i>=0;i--){
-        accumulator = (func(accumulator,arr[i]))
+    for (let i = arr.length - 1; i >= 0; i--) {
+        accumulator = func(accumulator, arr[i]);
     }
-    return accumulator
+    return accumulator;
 }

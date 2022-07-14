81a86e8f5a47d891ad28eba8db9a79a952f08a6d
karyun.cheung <karyun.cheung@gmail.com>
2022-07-14 01:30:25 +0100

    quantifiers

--- quantifiers.js
+++ quantifiers.js
@@ -1,27 +1,27 @@
-function every(arr,func){
-    let counter = 0
-    for(let i = 0;i<arr.length;i++){
-        if(!func(arr[i],i,arr)){
-            return false
+function every(arr, func) {
+    let counter = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (!func(arr[i], i, arr)) {
+            return false;
         }
     }
-    return true
+    return true;
 }
-function some(arr,func){
-    let counter = 0
-    for(let i = 0;i<arr.length;i++){
-        if(func(arr[i],i,arr)){
-            return true
+function some(arr, func) {
+    let counter = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (func(arr[i], i, arr)) {
+            return true;
         }
     }
-    return false
+    return false;
 }
-function none(arr,func){
-    let counter = 0
-    for(let i = 0;i<arr.length;i++){
-        if(func(arr[i],i,arr)){
-            return false
+function none(arr, func) {
+    let counter = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (func(arr[i], i, arr)) {
+            return false;
         }
     }
-    return true
+    return true;
 }

2128c31d8469ec5713415ec89e8b17140dcba3fd
karyun.cheung <karyun.cheung@gmail.com>
2022-07-15 02:02:07 +0100

    hard

--- harder-bigger-bolder-stronger.js
+++ harder-bigger-bolder-stronger.js
@@ -1,21 +1,18 @@
-export function generateLetters(){
-    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    for(let i=1;i<=120;i++){
-        let char = ''
-        char += possible.charAt(Math.floor(Math.random()*possible.length))
-        let elem = document.createElement('div')
-        elem.innerHTML = char
-        // let str =  "font-size:"+(i+10)+"px;"
-        // elem.setAttribute("style",str);
-        elem.style.fontSize = (i+10)+'px';
-        if (i <= 40 ){
+export function generateLetters() {
+    for (let i = 1; i <= 120; i++) {
+        let char = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".charAt(
+            Math.floor(Math.random() * 26)
+        );
+        let elem = document.createElement("div");
+        elem.innerHTML = char;
+        elem.style.fontSize = i + 10 + "px";
+        if (i <= 40) {
             elem.style.fontWeight = "300";
         } else if (i <= 80) {
             elem.style.fontWeight = "400";
         } else {
             elem.style.fontWeight = "600";
         }
-        document.body.append(elem)
-        console.log(i)
+        document.body.append(elem);
     }
 }

74fed37ab01f3e1e115b00e168b018c39afe8bbf
karyun.cheung <karyun.cheung@gmail.com>
2022-07-16 00:10:00 +0100

    mouse

--- mouse-trap.js
+++ mouse-trap.js
@@ -1,29 +1,72 @@
-let flag = true
+let flag = true;
 export const createCircle = () => {
-    document.addEventListener('click', (e) => {
-        let elem = document.createElement('div')
-        elem.className = 'circle'
-        elem.style.left = e.clientX - 25 + 'px'
-        elem.style.top = e.clientY - 25 + 'px'
-        if (flag) {
-            elem.style.background = 'white'
-        } else {
-            elem.style.background = 'var(--purple)'
-        }
-        document.body.appendChild(elem)
-        flag = true
-    },false)
-}
+    document.addEventListener(
+        "click",
+        (e) => {
+            let elem = document.createElement("div");
+            elem.className = "circle";
+            elem.style.left = e.clientX - 25 + "px";
+            elem.style.top = e.clientY - 25 + "px";
+            if (flag) {
+                elem.style.background = "white";
+            } else {
+                elem.style.background = "var(--purple)";
+            }
+            document.body.appendChild(elem);
+            flag = true;
+        },
+        false
+    );
+};
 export const moveCircle = () => {
-    document.addEventListener('mousemove', (e) => {
-        let elem = document.body.lastChild
-        if (elem === null) return
-        elem.style.left = e.clientX - 25 + 'px'
-        elem.style.top = e.clientY - 25 + 'px'
-    }, false)
-}
+    document.addEventListener(
+        "mousemove",
+        (e) => {
+            document.querySelectorAll(".circleRem").forEach((el) => {
+                el.remove();
+            });
+            let elem = document.createElement("div");
+            elem.className = "circle";
+            elem.classList.add("circleRem");
+            if (flag) elem.style.background = "white";
+            else elem.style.background = "var(--purple)";
+            elem.style.left = e.clientX - 25 + "px";
+            elem.style.top = e.clientY - 25 + "px";
+            document.body.appendChild(elem);
+            if (
+                e.clientX >= box.getBoundingClientRect().left + 25 &&
+                e.clientX <= box.getBoundingClientRect().right - 25 &&
+                e.clientY >= box.getBoundingClientRect().top + 25 &&
+                e.clientY <= box.getBoundingClientRect().bottom - 25
+            ) {
+                document.querySelector(".circle").style.background =
+                    "var(--purple)";
+                flag = false;
+            }
+            if (!flag) {
+                if (e.clientX - 25 < box.getBoundingClientRect().left) {
+                    circle.style.left = box.getBoundingClientRect().left + "px";
+                }
+                if (e.clientX + 25 > box.getBoundingClientRect().right) {
+                    circle.style.left =
+                        box.getBoundingClientRect().right - 50 + "px";
+                }
+                if (e.clientY - 25 < box.getBoundingClientRect().top) {
+                    circle.style.top = box.getBoundingClientRect().top + "px";
+                }
+                if (e.clientY + 25 > box.getBoundingClientRect().bottom) {
+                    circle.style.top =
+                        box.getBoundingClientRect().bottom - 50 + "px";
+                }
+                document.querySelector(".circle").style.background =
+                    "var(--purple)";
+            }
+        },
+        false
+    );
+};
 export const setBox = () => {
-    let elem = document.createElement('div')
-    elem.className = 'box'
-    document.body.appendChild(elem)
-}
+    let elem = document.createElement("div");
+    elem.className = "box";
+    document.body.appendChild(elem);
+};

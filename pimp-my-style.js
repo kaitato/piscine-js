23e12f50d3bf25ee8ce31cc8748d697cb6c17d7f
karyun.cheung <karyun.cheung@gmail.com>
2022-07-15 14:52:51 +0100

    pimp

--- pimp-my-style.js
+++ pimp-my-style.js
@@ -1,21 +1,24 @@
 import { styles } from "./pimp-my-style.data.js";
 
-export const pimp = e => {
-    let button = e.srcElement;
-    if (!button.classList.contains('unpimp')) {
-        button.classList.add(styles[button.classList.length-1]);
-        if (button.classList.length === styles.length + 1 && !button.classList.contains('unpimp')) {
-            button.classList.toggle('unpimp');
+export const pimp = (e) => {
+    let b = e.querySelector("button");
+    if (b.classList.contains("unpimp")) {
+        let classes = Array.from(b.classList);
+        b.classList.remove(classes[classes.length - 2]);
+        if (b.classList.length === 2) {
+            b.classList.toggle("unpimp");
+            return;
+        }
+    }
+    if (!b.classList.contains("unpimp")) {
+        b.classList.add(styles[b.classList.length - 1]);
+        if (
+            b.classList.length === styles.length + 1 &&
+            !b.classList.contains("unpimp")
+        ) {
+            b.classList.toggle("unpimp");
             return;
         }
         return;
     }
-    if (button.classList.contains('unpimp')) {
-        let classes = Array.from(button.classList);
-        button.classList.remove(classes[classes.length-2]);
-        if (button.classList.length === 2) {
-            button.classList.toggle('unpimp');
-            return;
-        }
-    }
-} 
+};

a16ecb3764c10425f9c7854c052803d4f1d036cd
karyun.cheung <karyun.cheung@gmail.com>
2022-07-15 01:56:06 +0100

    build brick

--- build-brick-and-break.js
+++ build-brick-and-break.js
@@ -1,42 +1,32 @@
-export function build(x){
-    let i = 1
-    let block = ''
-    let counterFoundation = 2
-    let interval = setInterval(function(){
-        block = document.createElement('div')
-        document.body.append(block)
-        block.innerHTML = i
-        block.id = 'brick-'+i
-        ++i
-        if(counterFoundation === 3){
-        block.dataset.foundation = true
-        counterFoundation = 0
-        }
-        ++counterFoundation
-        console.log(block)
-        if(i===x+1){
-            clearInterval(interval)
-        }
-    },100,i)
-    return block
+export function build(x) {
+    let i = 1;
+    let m = 2;
+    let interval = setInterval(function () {
+        let block = document.createElement("div");
+        block.id = "brick-" + i;
+        document.body.append(block);
+        if (i === m) {
+            block.dataset.foundation = true;
+            m += 3;
+        } else if (i === x) clearInterval(interval);
+        i++;
+    }, 100);
 }
-export function repair(...repairs){
+export function repair(...repairs) {
     for (let i = 0; i < repairs.length; i++) {
-        let elem = document.getElementById(repairs[i])
-        // console.log(elem.getAttribute("foundation"))
-        let n = repairs[i].replace('brick-', '')
-        if(n%3===2){
-            elem.setAttribute('data-repaired',"in progress")
-            elem.innerHTML = n
-        }else{
-            elem.setAttribute("data-repaired","true")
-            elem.innerHTML = n
+        let elem = document.getElementById(repairs[i]);
+        let n = repairs[i].replace("brick-", "");
+        if (n % 3 === 2) {
+            elem.setAttribute("data-repaired", "in progress");
+            elem.innerHTML = n;
+        } else {
+            elem.setAttribute("data-repaired", "true");
+            elem.innerHTML = n;
         }
-        // console.log(Number(repairs[i]))
     }
 }
 
-export function destroy(){
-    let elements = document.querySelectorAll('div[id^="brick"]')
-    elements[elements.length-1].remove()
+export function destroy() {
+    let elements = document.querySelectorAll('div[id^="brick"]');
+    elements[elements.length - 1].remove();
 }

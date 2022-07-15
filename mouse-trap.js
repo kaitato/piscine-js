7f70437617027ddeffac331f5e95f0b473e5c9a5
karyun.cheung <karyun.cheung@gmail.com>
2022-07-16 00:53:24 +0100

    ay

--- mouse-trap.js
+++ mouse-trap.js
@@ -2,28 +2,29 @@ let box;
 let x;
 let y;
 let flag = true;
-let elem = document.createElement("div");
 export function createCircle() {
     addEventListener("click", function () {
+        let elem = document.createElement("div");
         elem.className = "circle";
-        elem.style.left = x;
-        elem.style.top = y;
         if (flag) {
             elem.style.background = "white";
         } else {
             elem.style.background = "var(--purple)";
         }
+        elem.style.left = x;
+        elem.style.top = y;
         document.body.appendChild(elem);
         flag = true;
     });
 };
 export function moveCircle() {
-    addEventListener("mousemove", (e) => {
+    addEventListener("mousemove", e => {
         document.querySelectorAll(".circleRem").forEach((el) => {
             el.remove();
         });
         x = e.clientX - 25 + "px";
         y = e.clientY - 25 + "px";
+        let elem = document.createElement("div");
         elem.className = "circle";
         elem.classList.add("circleRem");
         if (flag) {
@@ -47,20 +48,27 @@ export function moveCircle() {
         if (!flag) {
             if (e.clientX - 25 < box.getBoundingClientRect().left) {
                 circle.style.left = box.getBoundingClientRect().left + "px";
+                document.querySelector(".circle").style.background =
+                "var(--purple)";
             }
             if (e.clientX + 25 > box.getBoundingClientRect().right) {
                 circle.style.left =
                     box.getBoundingClientRect().right - 50 + "px";
+                    document.querySelector(".circle").style.background =
+                "var(--purple)";
             }
             if (e.clientY - 25 < box.getBoundingClientRect().top) {
                 circle.style.top = box.getBoundingClientRect().top + "px";
+                document.querySelector(".circle").style.background =
+                "var(--purple)";
             }
             if (e.clientY + 25 > box.getBoundingClientRect().bottom) {
                 circle.style.top =
                     box.getBoundingClientRect().bottom - 50 + "px";
-            }
-            document.querySelector(".circle").style.background =
+                    document.querySelector(".circle").style.background =
                 "var(--purple)";
+            }
+            
         }
     });
 };

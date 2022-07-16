151e985935638f1c75dab841efe4e931cc72ee64
karyun.cheung <karyun.cheung@gmail.com>
2022-07-16 01:24:08 +0100

    pick

--- pick-and-click.js
+++ pick-and-click.js
@@ -1,11 +1,13 @@
-export const pick = () => {
-    let elem = document.createElement("div");
-    elem.className = "text";
-    elem.classList.add("hsl");
-    document.body.appendChild(elem);
+export function pick() {
+    let div1 = document.createElement("div");
+    div1.className = "text";
+    div1.classList.add("hsl");
+    document.body.appendChild(div1);
+
     let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
     svg.setAttributeNS(null, "height", window.innerHeight);
     svg.setAttributeNS(null, "width", window.innerWidth);
+
     let linex = document.createElementNS("http://www.w3.org/2000/svg", "line");
     linex.setAttributeNS(null, "id", "axisY");
     linex.setAttributeNS(null, "x1", 0);
@@ -14,6 +16,7 @@ export const pick = () => {
     linex.setAttributeNS(null, "y2", 0);
     linex.style.stroke = "white";
     linex.style.strokeWidth = 1;
+
     let liney = document.createElementNS("http://www.w3.org/2000/svg", "line");
     liney.setAttributeNS(null, "id", "axisX");
     liney.setAttributeNS(null, "x1", 0);
@@ -22,25 +25,35 @@ export const pick = () => {
     liney.setAttributeNS(null, "y2", 0);
     liney.style.stroke = "white";
     liney.style.strokeWidth = 1;
+
     svg.appendChild(liney);
     svg.appendChild(linex);
+
     document.body.appendChild(svg);
+
     addEventListener("mousemove", (e) => {
-        document
-            .querySelectorAll(".hue", ".hsl", ".luminosity")
-            .forEach((e) => {
-                e.remove();
-            });
+        document.querySelectorAll(".hue", ".hsl").forEach((e) => {
+            e.remove();
+        });
+        document.querySelectorAll(".hsl").forEach((e) => {
+            e.remove();
+        });
+        document.querySelectorAll(".luminosity").forEach((e) => {
+            e.remove();
+        });
         let liney = document.getElementById("axisX");
         let linex = document.getElementById("axisY");
+
         linex.setAttributeNS(null, "x1", 0);
         linex.setAttributeNS(null, "y1", e.clientY);
         linex.setAttributeNS(null, "x2", window.innerWidth);
         linex.setAttributeNS(null, "y2", e.clientY);
+
         liney.setAttributeNS(null, "x1", e.clientX);
         liney.setAttributeNS(null, "y1", 0);
         liney.setAttributeNS(null, "x2", e.clientX);
         liney.setAttributeNS(null, "y2", window.innerHeight);
+
         let x = (e.clientX / window.innerWidth) * 360;
         let y = (e.clientY / window.innerHeight) * 100;
         document.body.style.background = "hsl(" + x + ",50%," + y + "%)";
@@ -49,12 +62,14 @@ export const pick = () => {
         hue.classList.add("text");
         hue.innerHTML = "hue" + "<br>" + Math.round(x);
         document.body.appendChild(hue);
+
         let hsl = document.createElement("div");
         hsl.className = "hsl";
         hsl.classList.add("text");
         hsl.innerHTML =
             "hsl(" + Math.round(x) + ", 50%, " + Math.round(y) + "%)";
         document.body.appendChild(hsl);
+
         let luminosity = document.createElement("div");
         luminosity.className = "luminosity";
         luminosity.classList.add("text");
@@ -70,4 +85,4 @@ export const pick = () => {
         document.execCommand("copy");
         document.body.removeChild(input);
     });
-};
+}

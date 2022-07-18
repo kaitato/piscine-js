ec436d566a5f53dba3640bb99464aefe3da5a65c
karyun.cheung <karyun.cheung@gmail.com>
2022-07-18 19:14:04 +0100

    pronoun

--- pronoun.js
+++ pronoun.js
@@ -1,18 +1,21 @@
-const pronouns = ['i','you','he','she','it','we','they']
+const pronouns = ["i", "you", "he", "she", "it", "we", "they"];
 
 const pronoun = (str) => {
-    let res = {}
-    let arr = str.toLowerCase().match(/\w+/ig)
-    arr.forEach((v,i)=> {
+    let res = {};
+    let arr = str.toLowerCase().match(/\w+/gi);
+    arr.forEach((v, i) => {
         if (pronouns.includes(v.toLowerCase())) {
             if (!(v in res)) {
-                res[v] = {word:[],count:0}
+                res[v] = { word: [], count: 0 };
             }
-            if (arr[i+1] !== undefined && !(pronouns.includes(arr[i+1].toLowerCase()))) {
-                res[v]['word'].push(arr[i+1])
-            } 
-            res[v]['count'] += 1
+            if (
+                arr[i + 1] !== undefined &&
+                !pronouns.includes(arr[i + 1].toLowerCase())
+            ) {
+                res[v]["word"].push(arr[i + 1]);
+            }
+            res[v]["count"] += 1;
         }
-    })
-    return res
-}
+    });
+    return res;
+};

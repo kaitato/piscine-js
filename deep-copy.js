eef6c63d18076f8858bf545afb98885eb4819463
karyun.cheung <karyun.cheung@gmail.com>
2022-07-18 19:11:30 +0100

    deep copy

--- deep-copy.js
+++ deep-copy.js
@@ -1,15 +1,15 @@
-const superTypeOf = (arg) => toString.call(arg).slice(8, -1)
+const superTypeOf = (arg) => toString.call(arg).slice(8, -1);
 
 const deepCopy = (arg) => {
-    let res
-    if (superTypeOf(arg) == 'Object') {
-        res = {}
-        Object.keys(arg).forEach((k)=>res[k]=deepCopy(arg[k]))
-    } else if (superTypeOf(arg)== 'Array') {
-        res = []
-        arg.forEach((v,i)=>res[i]=deepCopy(arg[i]))
+    let res;
+    if (superTypeOf(arg) == "Object") {
+        res = {};
+        Object.keys(arg).forEach((k) => (res[k] = deepCopy(arg[k])));
+    } else if (superTypeOf(arg) == "Array") {
+        res = [];
+        arg.forEach((v, i) => (res[i] = deepCopy(arg[i])));
     } else {
-        res = arg
+        res = arg;
     }
-    return res
-}
+    return res;
+};

0d16f5ebc85dce5d75f7515fb19934e5082ab7e9
karyun.cheung <karyun.cheung@gmail.com>
2022-07-18 18:51:21 +0100

    pick omit

--- pick-omit.js
+++ pick-omit.js
@@ -1,16 +1,23 @@
 const pick = (object, string) => {
     let keys = Object.keys(object);
-    let filteredKeys = keys.filter(key => typeof string === "string" ? string === key : string.includes(key))
-    let e = {}
-    filteredKeys.forEach((filteredKey) => e[filteredKey] = object[filteredKey])
-    return e
-}
+    let filteredKeys = keys.filter((key) =>
+        typeof string === "string" ? string === key : string.includes(key)
+    );
+    let e = {};
+    filteredKeys.forEach(
+        (filteredKey) => (e[filteredKey] = object[filteredKey])
+    );
+    return e;
+};
 
 const omit = (object, string) => {
     let keys = Object.keys(object);
-    let filteredKeys = keys.filter(key => typeof string === "string" ? !(string === key) : !(string.includes(key)))
-    let e = {}
-    filteredKeys.forEach((filteredKey) => e[filteredKey] = object[filteredKey])
-    return e
-}
-
+    let filteredKeys = keys.filter((key) =>
+        typeof string === "string" ? !(string === key) : !string.includes(key)
+    );
+    let e = {};
+    filteredKeys.forEach(
+        (filteredKey) => (e[filteredKey] = object[filteredKey])
+    );
+    return e;
+};

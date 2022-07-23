a912d871d4e217495c365744dc7aeab8b35ab874
karyun.cheung <karyun.cheung@gmail.com>
2022-07-23 14:38:21 +0100

    interpolation

--- interpolation.js
+++ interpolation.js
@@ -1,11 +1,10 @@
-function interpolation ({step, start, end, callback, duration}) {
+function interpolation({ step, start, end, callback, duration }) {
     let x = start;
-    let y = duration/step;
+    let y = duration / step;
     let timer = setInterval(() => {
-        callback([x,y]);
-        x += (end - start) / step
-        y += duration / step
+        callback([x, y]);
+        x += (end - start) / step;
+        y += duration / step;
         if (Math.round(x * 100) / 100 === end) clearInterval(timer);
     }, duration / step);
-
 }

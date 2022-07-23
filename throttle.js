1babad9ba68ad4c41522df29769bde90e616537c
karyun.cheung <karyun.cheung@gmail.com>
2022-07-23 14:37:44 +0100

    throttle

--- throttle.js
+++ throttle.js
@@ -1,55 +1,53 @@
 function throttle(functime, wait) {
-  let Throttleok, savedArgs, saved
+    let Throttleok, savedArgs, saved;
 
-  function wrap() {
+    function wrap() {
+        if (Throttleok) {
+            savedArgs = arguments;
+            saved = this;
+            return;
+        }
 
-      if (Throttleok) {
-          savedArgs = arguments
-          saved = this
-          return
-      }
+        Throttleok = true;
+        functime.apply(this, arguments);
 
-      Throttleok = true;
-      functime.apply(this, arguments);
+        setTimeout(function () {
+            Throttleok = false;
+            if (savedArgs) {
+                wrap.apply(saved, savedArgs);
+                savedArgs = saved = null;
+            }
+        }, wait);
+    }
 
-      setTimeout(function () {
-          Throttleok = false
-          if (savedArgs) {
-              wrap.apply(saved, savedArgs)
-              savedArgs = saved = null
-          }
-      }, wait)
-  }
-
-  return wrap;
-
-};
+    return wrap;
+}
 function opThrottle(functime, wait, options) {
-  let timer, savedArgs, saved
+    let timer, savedArgs, saved;
 
-  return function () {
-      if (timer) {
-          saved = this
-          savedArgs = arguments
-          return
-      }
+    return function () {
+        if (timer) {
+            saved = this;
+            savedArgs = arguments;
+            return;
+        }
 
-      const timeup = () => {
-          if (options?.trailing === true && savedArgs) {
-              functime.apply(saved, savedArgs)
-              saved = savedArgs = null
-              timer = setTimeout(timeup, wait)
-          } else {
-              timer = null
-          }
-      }
+        const timeup = () => {
+            if (options?.trailing === true && savedArgs) {
+                functime.apply(saved, savedArgs);
+                saved = savedArgs = null;
+                timer = setTimeout(timeup, wait);
+            } else {
+                timer = null;
+            }
+        };
 
-      if (options?.leading === true) {
-          functime.apply(this, arguments)
-      } else {
-          saved = this
-          savedArgs = arguments
-      }
-      timer = setTimeout(timeup, wait)
-  }
+        if (options?.leading === true) {
+            functime.apply(this, arguments);
+        } else {
+            saved = this;
+            savedArgs = arguments;
+        }
+        timer = setTimeout(timeup, wait);
+    };
 }
